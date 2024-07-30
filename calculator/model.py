e52c52c8f8c1755f47d2b82e88ce753a4f663d67
Tomás Buriticá <tomasburitica17@gmail.com>
2024-07-30 11:15:46 -0500

    Add functions of the calculator

---
+++ calculator/model.py
@@ -0,0 +1,14 @@
+class Calculator:
+    def add(self, a: float, b: float) -> float:
+        return a + b
+
+    def substract(self, a: float, b: float) -> float:
+        return a - b
+
+    def multiply(self, a: float, b: float) -> float:
+        return a * b
+
+    def divition(self, a: float, b: float) -> float:
+        if b == 0:
+            return "cannot divide by 0"
+        return a / b
