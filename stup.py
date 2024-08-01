cdc507ddfe6c33a3b42634a83cc0c555c8cd54c6
Tomás Buriticá <tomasburitica17@gmail.com>
2024-08-01 10:40:35 -0500

    Add setup.py and requirements.txt

---
+++ stup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+
+    name="cli-calculator",
+    version="0.1",
+    packages=['calculator','tests'],
+    install_requirements=[
+        'click'
+    ],
+    entry_points='''
+    [console_scripts]
+    calc=calculator.cli:calc
+    '''
+)
